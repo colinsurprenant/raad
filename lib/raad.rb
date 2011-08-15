2614dd53e98351002c43088b27ffc63aff59a96c
Colin Surprenant <colin.surprenant@gmail.com>
2011-08-15 14:00:13 -0400

    load paths

--- lib/raad.rb
+++ lib/raad.rb
@@ -1,6 +1,11 @@
 # Copyright (c) 2011 Praized Media Inc.
 # Author: Colin Surprenant (colin.surprenant@needium.com, colin.surprenant@gmail.com, @colinsurprenant, http://github.com/colinsurprenant)
 
+module Raad
+  Root = File.dirname(__FILE__)
+  $:.unshift Root
+end
+
 require 'rubygems'
 require 'lib/raad/version'
 require 'lib/raad/raad'
