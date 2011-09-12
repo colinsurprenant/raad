da4a5e577eb9c84833af0b1306d380c20f7d3856
Colin Surprenant <colin.surprenant@gmail.com>
2011-09-12 17:27:21 -0400

    fixed race condition in thread test

--- spec/raad/signal_trampoline_spec.rb
+++ spec/raad/signal_trampoline_spec.rb
@@ -1,16 +1,18 @@
 require 'spec_helper'
 require 'raad/signal_trampoline'
 require 'thread'
+require 'timeout'
 
 describe SignalTrampoline do
   it "should trap signal" do
     t = Thread.new{Thread.stop}
     SignalTrampoline.trap(:USR1) {t.run}
     t.alive?.should == true
-    t.stop?.should == true
+    Timeout.timeout(5) {sleep(0.1) while !t.stop?} # avoid race condition
+    t.stop?.should be_true
     Process.kill(:USR1, Process.pid)
     t.join(2).should == t
-    t.alive?.should == false
+    t.alive?.should be_false
   end
 
   it "should raise on bad signal" do
