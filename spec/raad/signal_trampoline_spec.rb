ddb7b7ce34422ddc29b7c31df1b76d6935ef522e
Colin Surprenant <colin.surprenant@gmail.com>
2011-09-13 01:55:19 -0400

    ajusted test for REE for which I must dig for a possible threading bug

--- spec/raad/signal_trampoline_spec.rb
+++ spec/raad/signal_trampoline_spec.rb
@@ -7,12 +7,13 @@ describe SignalTrampoline do
   it "should trap signal" do
     t = Thread.new{Thread.stop}
     SignalTrampoline.trap(:USR1) {t.run}
-    t.alive?.should == true
+    t.alive?.should be_true
     Timeout.timeout(5) {sleep(0.1) while !t.stop?} # avoid race condition
     t.stop?.should be_true
     Process.kill(:USR1, Process.pid)
-    t.join(2).should == t
+    Timeout.timeout(5) {sleep(0.1) while t.alive?} 
     t.alive?.should be_false
+    t.join(5).should == t
   end
 
   it "should raise on bad signal" do
